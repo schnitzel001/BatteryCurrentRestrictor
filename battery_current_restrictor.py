26410274fb680552daf8a5861735cc8a497a1a21
schnitzel001 <fritz.schurig@gmx.de>
2026-03-31 16:41:20 +0200

    improved limitation logic

--- battery_current_restrictor.py
+++ battery_current_restrictor.py
@@ -16,7 +16,8 @@ class BatteryCurrentRestrictor:
     def __init__(self):
         DBusGMainLoop(set_as_default=True)
         self.bus = dbus.SystemBus()
-        #self.config = load_config()
+        self.config = load_config()
+        self.calculated_sp = 0
 
     def get_value(self, service, path):
         try:
@@ -34,58 +35,66 @@ class BatteryCurrentRestrictor:
             print("Write error:", e)
 
     def calculate(self, grid_power, battery_power, max_charge_power):
-        target = 0
-        # if battery_power exceeds allowed max_charge_power calculate difference and use it as new set point
-        if battery_power > max_charge_power:
-            delta = battery_power - max_charge_power
-            target = grid_power - 1.1 * delta
+        # calculate difference and use it as new set point
+        delta = battery_power - max_charge_power
+        target = grid_power - delta
         # restrict to absolute limits
         target = max(target, self.config["min_setpoint"])
-        target = min(target, self.config["max_setpoint"])
+        target = min(target, self.config["max_setpoint"]) # max should be 0!
         return int(target)
 
     def run(self):
-        print("Battery current restritor started")
+        print("Battery current restrictor started")
 
         #store default set point
-        default_sp = self.get_value("com.victronenergy.settings",
-                                    "/Settings/CGwacs/AcPowerSetPoint")
+        default_sp = 0
+        limitation_active = False
 
         while True:
-            soc = self.get_value("com.victronenergy.battery.socketcan_can1",
-                                 "/Soc")
-            battery_charging = self.get_value("com.victronenergy.battery.socketcan_can1","/Dc/0/Power") > 0
-            # only perform restriction when SOC<100% and battery is currently charging
-            if soc < 100 and battery_charging:
-                L1_grid_power = self.get_value("com.victronenergy.system","/Ac/Grid/L1/Power")
-                L2_grid_power = self.get_value("com.victronenergy.system","/Ac/Grid/L2/Power")
-                L3_grid_power = self.get_value("com.victronenergy.system","/Ac/Grid/L3/Power")
-                grid_power = L1_grid_power + L2_grid_power + L3_grid_power
-                battery_power = self.get_value("com.victronenergy.system","/Dc/Battery/Power")
-                max_charge_current = self.get_value("com.victronenergy.battery.socketcan_can1",
-                                                    "/Info/MaxChargeCurrent")
-                dc_voltage = self.get_value("com.victronenergy.battery.socketcan_can1",
-                                                    "/Dc/0/Voltage")
-                max_charge_power =  max_charge_current * dc_voltage
-                if None in (grid_power, battery_power, max_charge_power):
-                    print("Error on reading values:")
-                    print(f"Grid={grid_power}W, Battery={battery_power}W, Limit={max_charge_power}W")
-                    time.sleep(self.config["interval"])
-                    continue
-                new_sp = self.calculate(grid_power, battery_power, max_charge_power)
-                self.set_value("com.victronenergy.settings",
-                               "/Settings/CGwacs/AcPowerSetPoint",
-                                new_sp
-                )
-                print(f"Grid={grid_power}W, Battery={battery_power}W, Limit={max_charge_power}W → SP={new_sp}W")
+            soc = int(self.get_value("com.victronenergy.battery.socketcan_can1","/Soc"))
+            battery_power = int(self.get_value("com.victronenergy.system", "/Dc/Battery/Power"))
 
-            #otherwise restore default grid set point
-            else:
+            max_charge_current = self.get_value("com.victronenergy.battery.socketcan_can1",
+                                                "/Info/MaxChargeCurrent")
+            dc_voltage = self.get_value("com.victronenergy.battery.socketcan_can1",
+                                        "/Dc/0/Voltage")
+            max_charge_power = int(max_charge_current * dc_voltage)
+
+            L1_grid_power = self.get_value("com.victronenergy.system", "/Ac/Grid/L1/Power")
+            L2_grid_power = self.get_value("com.victronenergy.system", "/Ac/Grid/L2/Power")
+            L3_grid_power = self.get_value("com.victronenergy.system", "/Ac/Grid/L3/Power")
+            grid_power = int(L1_grid_power + L2_grid_power + L3_grid_power)
+
+            # only perform restriction when SOC<100%
+            if soc < 100:
+                # check if already in limiting mode
+                if limitation_active:
+                    # calculate new set point and set this value
+                    self.calculated_sp = self.calculate(grid_power, battery_power, max_charge_power)
+                    self.set_value("com.victronenergy.settings",
+                                   "/Settings/CGwacs/AcPowerSetPoint",
+                                   self.calculated_sp
+                                   )
+                    print(f"Grid={grid_power}W, Battery={battery_power}W, Limit={max_charge_power}W → SP={self.calculated_sp}W")
+
+                    # if new set point matches default, limitation can be deactivated
+                    if self.calculated_sp == default_sp:
+                        limitation_active = False
+                        print("Limitation got deactivated")
+
+                # check if limitation needs to be activated
+                elif battery_power > max_charge_power:
+                    limitation_active = True
+                    print("Limitation got activated")
+
+            #otherwise restore default grid set point and deactivate limitation if still active
+            elif limitation_active:
                 self.set_value("com.victronenergy.settings",
                                "/Settings/CGwacs/AcPowerSetPoint",
                                default_sp
                                )
-
+                limitation_active = False
+                print("Limitation got deactivated")
 
             time.sleep(self.config["interval"])
 
