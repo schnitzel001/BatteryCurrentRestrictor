02f9a61a361b467699ef8cbff2c0e9d065adbda4
schnitzel001 <fritz.schurig@gmx.de>
2026-04-01 22:21:43 +0200

    implemented charge curve limit

--- battery_current_restrictor.py
+++ battery_current_restrictor.py
@@ -63,6 +63,51 @@ class BatteryCurrentRestrictor:
         target = min(target, self.config["max_setpoint"]) # max should be 0!
         return int(target)
 
+    def get_charge_limit(self, soc):
+        list_of_limits =[]
+        # from BMS
+        if self.config.get("bms_limit_activated"):
+            bms_limit = self.get_value(self.config.get("bms_limit_dbus_service"),
+                                       self.config.get("bms_limit_value_path"))
+            list_of_limits.append(bms_limit)
+
+        # from configured charge curve
+        if self.config.get("charge_curve_limit_activated"):
+            curve = self.config.get("charge_curve", [])
+            charge_curve_limit = self.get_allowed_current_from_curve(soc, curve)
+            list_of_limits.append(charge_curve_limit)
+
+        # based on forecast
+        #ToDo: implementation missing
+
+        if list_of_limits:
+            return min(list_of_limits)
+        else:
+            return -1
+
+
+    def get_allowed_current_from_curve(self, soc, curve):
+        # sort values
+        curve = sorted(curve, key=lambda x: x["soc"])
+
+        # edge-cases
+        if soc <= curve[0]["soc"]:
+            return curve[0]["current"]
+        if soc >= curve[-1]["soc"]:
+            return curve[-1]["current"]
+
+        # interpolate
+        for i in range(len(curve) - 1):
+            p1 = curve[i]
+            p2 = curve[i + 1]
+
+            if p1["soc"] <= soc <= p2["soc"]:
+                soc_range = p2["soc"] - p1["soc"]
+                current_range = p2["current"] - p1["current"]
+                factor = (soc - p1["soc"]) / soc_range
+
+                return p1["current"] + factor * current_range
+
     def run(self):
         logger.info("Battery current restrictor started")
 
@@ -74,8 +119,11 @@ class BatteryCurrentRestrictor:
             soc = int(self.get_value("com.victronenergy.battery.socketcan_can1","/Soc"))
             battery_power = int(self.get_value("com.victronenergy.system", "/Dc/Battery/Power"))
 
-            max_charge_current = self.get_value("com.victronenergy.battery.socketcan_can1",
-                                                "/Info/MaxChargeCurrent")
+            max_charge_current = self.get_charge_limit(soc)
+            if max_charge_current == -1:
+                # no limit activated - skip all
+                continue
+
             dc_voltage = self.get_value("com.victronenergy.battery.socketcan_can1",
                                         "/Dc/0/Voltage")
             max_charge_power = int(max_charge_current * dc_voltage)
@@ -101,11 +149,13 @@ class BatteryCurrentRestrictor:
                     if self.calculated_sp == default_sp:
                         limitation_active = False
                         logger.info("Limitation got deactivated")
+                        continue
 
                 # check if limitation needs to be activated
                 elif battery_power > max_charge_power:
                     limitation_active = True
                     logger.info("Limitation got activated")
+                    continue
 
             #otherwise restore default grid set point and deactivate limitation if still active
             elif limitation_active:
