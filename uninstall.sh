51baf5c5be7ca7107fe2f6d7266fe47c5b5f14c5
schnitzel001 <fritz.schurig@gmx.de>
2026-04-13 21:06:50 +0200

    Add uninstall script for service removal and cleanup

---
+++ uninstall.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SERVICE_NAME=$(basename $SCRIPT_DIR)
+LOG_DIR=/var/log/$SERVICE_NAME
+RC_LOCAL_FILE=/data/rc.local
+
+#remove the service
+if [ -d /service/$SERVICE_NAME ]; then  
+    rm /service/$SERVICE_NAME
+fi 
+
+# end the dbus-opendtu process
+kill $(pgrep -f "supervise $SERVICE_NAME\$")
+kill $(pgrep -f "python $SCRIPT_DIR/battery_current_restrictor.py")
+
+# delete old logs if they exist  
+if [ -f $SCRIPT_DIR/app.log ]; then  
+    rm $SCRIPT_DIR/app.log*  
+fi 
+
+# remove install.sh from rc.local
+STARTUP=$SCRIPT_DIR/install.sh
+sed -i "\~$STARTUP~d" $RC_LOCAL_FILE
+
+# delete log folder in var log if they exist  
+if [ -d $LOG_DIR ]; then  
+    while true; do
+        read -p "Do you really wish to delete the log folder $LOG_DIR? [y/n]" yn
+        case $yn in
+            [Yy]* ) rm -rf $LOG_DIR; echo $LOG_DIR is deleted ; break;;
+            [Nn]* ) break;;
+            * ) echo "Please answer y or n.";;
+        esac
+    done
+fi 
